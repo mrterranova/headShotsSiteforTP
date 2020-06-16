76788cbe0fb12b228d9294e8acd2f2cc5e294325
BLoveless <bloveless@talentpath.com>
2020-06-16 16:54:00 -0400

    start

---
+++ headshotz/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+<h1>Welcome to Talent Paths Head shots web app</h1>
+    </div>
+  );
+}
+
+export default App;
