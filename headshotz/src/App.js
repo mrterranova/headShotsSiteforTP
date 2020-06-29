ca0a9ff7f4706260db6a1a10d69d32d77b6ba600
JC <avalojc@gmail.com>
2020-06-29 14:30:13 -0400

    created nav bar, added nav bar to route, created empty home component, added home component to routes. assigned route names

--- headshotz/src/App.js
+++ headshotz/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Cam from './componets/camera';
 import ImagePreview from './componets/ImagePreview';
+import NavBar from './componets/navBar'
+import Home from './componets/home';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
@@ -8,9 +10,12 @@ function App() {
   return (
     <Router>
       <h1>Headshot Application</h1>
+      <NavBar/>
       <Switch>
-          <Route path="/" exact component={Cam} />
-          <Route path="/" exact component={ImagePreview} /> 
+          <Route path="/" exact component={Home} />  
+          <Route path="/home" exact component={Home} />  
+          <Route path="/camera" exact component={Cam} />
+          <Route path="/camera" exact component={ImagePreview} /> 
       </Switch>
     </Router>
 
---
+++ headshotz/src/componets/navBar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+
+class NavBar extends Component {
+    render () {
+
+        return (
+            <div className="fullNavUnit">
+                <div className="navWrapper">
+                    <Link to="/home" ><div className="linkBucket" >Home</div> </Link>
+                    <Link to="/camera"><div className="linkBucket" >Camera</div></Link>
+                </div>
+            </div>
+            )
+        }
+}
+
+export default NavBar
