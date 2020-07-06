dab8a58d414abf5cf04836d6e8892c85eb5b5778
BLoveless <bloveless@talentpath.com>
2020-07-06 10:34:51 -0400

    work around

--- src/components/LineItems.jsx
+++ src/components/LineItems.jsx
@@ -3,7 +3,7 @@ import LineItem0 from './LineItem0'
 import LineItem1 from './LineItem1'
 import LineItem2 from './LineItem2'
 import LineItem3 from './LineItem3'
-import LineItem4 from './LineItem4'
+import LineItem4 from './LineItem4_1'
 
 class LineItems extends Component {
     render () {
