bfc867a906f387837b786e4b11494df389d6656b
Bilikis Orulebaja <borulebaja@talentpath.com>
2020-07-06 10:33:27 -0500

    components 8,9,10

---
+++ src/components/LineItem10.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class LineItem10 extends Component {
+    render () {
+    return (
+        <div className="lineItem">
+        10. If not click retake and follow steps 5 through 10 again.
+        </div>
+        )
+    }
+}
+
+export default LineItem10
---
+++ src/components/LineItem9.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class LineItem9 extends Component {
+    render () {
+    return (
+        <div className="lineItem">
+        9. If you like the photo, then save it to your device and send it to your ************
+        </div>
+        )
+    }
+}
+
+export default LineItem9
