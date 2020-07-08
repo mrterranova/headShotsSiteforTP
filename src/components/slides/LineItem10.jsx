4050ef3043da3274288721c585a934cb6b236bb3
Farrah Carter <FCarter@talentpath.com>
2020-07-08 12:48:46 -0400

    Files renamed

--- src/components/slides/LineItem10.jsx
+++ src/components/slides/LineItem10.jsx
@@ -8,7 +8,7 @@ export default class LineItem10 extends Component {
             <p>
                 10. If you like the photo, then save it to your device and send it to your manager.
             <br/>
-            <img src="img/outlook.svg.png" alt="Microsoft Outlook logo" className="responsive" />
+            <img src="img/Outlook.png" alt="Microsoft Outlook logo" className="responsive" />
             </p>
             
         </div>
